5ca384ab9b0c29661b878e9c28d719f0d5ceebdf
Zrokei <2842809454@qq.com>
2026-03-16 23:13:21 +0800

    fix：添加了.vue文件，修改了index.ts文件的内容

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,13 @@
 // 引入 VitePress 默认主题
 import DefaultTheme from 'vitepress/theme'
 import './style/index.css'
+import Mycomponent from "./components/Mycomponent.vue"
 
 export default {
   extends: DefaultTheme, // 继承默认主题
-  
+  enhanceApp({app}) { 
+    // 注册全局组件
+    app.component('Mycomponent' , Mycomponent)
+  }
 }
 
